2f8bd1ec71cbac4bc3c8d444d46f0dccbdbf8375
zhengyang <718350171@qq.com>
2016-01-22 23:06:47 +0800

    经过封装隔离，完成电机部分所有功能函数和扩展函数的编写

--- app/src/main/java/com/example/fuan/ioiotest/BaseActivity.java
+++ app/src/main/java/com/example/fuan/ioiotest/BaseActivity.java
@@ -1,5 +1,8 @@
 package com.example.fuan.ioiotest;
 
+import ioio.lib.api.DigitalInput;
+import ioio.lib.api.DigitalOutput;
+import ioio.lib.api.IOIO;
 import ioio.lib.api.PwmOutput;
 import ioio.lib.api.exception.ConnectionLostException;
 import ioio.lib.util.BaseIOIOLooper;
@@ -8,32 +11,34 @@ import ioio.lib.util.android.IOIOActivity;
 
 
 public class BaseActivity extends IOIOActivity{
-    private PwmOutput leftMotorIO1,leftMotorIO2,rightMotorIO1,rightMotorIO2;
-    private float leftMoterIO1Duty,leftMotorIO2Duty,rightMotorIO1Duty,rightMotorIO2Duty;
-
-//    @Override
-//    public void onCreate(Bundle savedInstanceState){
-//        super.onCreate(savedInstanceState);
-//    }
-
+    private PwmOutput leftMotorPwm1,leftMotorPwm2,rightMotorPwm1,rightMotorPwm2;
+    private float leftMoterPwmDuty1,leftMoterPwmDuty2,rightMotorPwmDuty1,rightMotorPwmDuty2;
 
     class Looper extends BaseIOIOLooper{
+        /*
+        * 函数名称：initMotor
+        * 函数功能：电机引脚GPIO口设置
+        */
         public void initMotor() throws ConnectionLostException,InterruptedException{
-            leftMotorIO1 = ioio_.openPwmOutput(11, 100);
-            leftMotorIO2 = ioio_.openPwmOutput(12, 100);
-            rightMotorIO1 = ioio_.openPwmOutput(13, 100);
-            rightMotorIO2 = ioio_.openPwmOutput(14, 100);
+            leftMotorPwm1 = ioio_.openPwmOutput(11, 100);
+            leftMotorPwm2 = ioio_.openPwmOutput(12, 100);
+            rightMotorPwm1 = ioio_.openPwmOutput(13, 100);
+            rightMotorPwm2 = ioio_.openPwmOutput(14, 100);
         }
 
         @Override
         public void setup() throws ConnectionLostException,InterruptedException{
             initMotor();
         }
+        /*
+        * 函数名称：setMotorDuty
+        * 功能:    电机pwm波和数字电平参数设置
+        */
         public void setMotorDuty()  throws ConnectionLostException,InterruptedException{
-            leftMotorIO1.setDutyCycle(leftMoterIO1Duty);
-            leftMotorIO2.setDutyCycle(leftMotorIO2Duty);
-            rightMotorIO1.setDutyCycle(rightMotorIO1Duty);
-            rightMotorIO2.setDutyCycle(rightMotorIO2Duty);
+            leftMotorPwm1.setDutyCycle(leftMoterPwmDuty1);
+            leftMotorPwm2.setDutyCycle(leftMoterPwmDuty2);
+            rightMotorPwm1.setDutyCycle(rightMotorPwmDuty1);
+            rightMotorPwm2.setDutyCycle(rightMotorPwmDuty2);
         }
 
         @Override
@@ -48,42 +53,138 @@ public class BaseActivity extends IOIOActivity{
     }
     /*
     * 函数名称：moveForward
-    * 功能    ：电机正转
-    *
+    * 功能    ：电机正转，可以调节速度，但是超过提前标定的速度阈值则不能转动
     */
     public void moveForward(int speed){
         if( (speed >=0) && (speed <= 100)){
-            leftMoterIO1Duty = speed/100.0f;
-            leftMotorIO2Duty = 0;
-            rightMotorIO1Duty = speed/100.0f;
-            rightMotorIO2Duty = 0;
+            leftMoterPwmDuty1 = speed/100.0f;
+            leftMoterPwmDuty2 = 0;
+            rightMotorPwmDuty1= speed/100.0f;
+            rightMotorPwmDuty2 = 0;
         }
-        else if( (speed < 0) && (speed >= -100)){
-            leftMoterIO1Duty = 0;
-            leftMotorIO2Duty = -speed/100.0f;
-            rightMotorIO1Duty = 0;
-            rightMotorIO2Duty = -speed/100.0f;
+        else if( (speed < 0) && (speed > 100)){
+            leftMoterPwmDuty1 = 0;
+            leftMoterPwmDuty2 = 0;
+            rightMotorPwmDuty1 = 0;
+            rightMotorPwmDuty2 = 0;
         }
     }
-    public void Backward(int speed){
-//        pwmChange = speed/100.0f;
-//        motorRotate_1=false;
-//        motorRotate_2=false;
-//        PwmOutput_1.setDutyCycle(pwmChange);
-//        MotorRefer_1.write(motorRotate_1);
-//        PwmOutput_2.setDutyCycle(pwmChange);
-//        MotorRefer_2.write(motorRotate_2);
-    }//电机反转
-    public void TurnCircle(int speed,boolean direction) throws ConnectionLostException,InterruptedException{
-//        pwmChange = speed/100.0f;
-//        motorRotate_1 = direction;
-//        motorRotate_2 = !direction;
-//        PwmOutput_1.setDutyCycle(pwmChange);
-//        MotorRefer_1.write(!motorRotate_1);
-//        PwmOutput_2.setDutyCycle(pwmChange);
-//        MotorRefer_2.write(motorRotate_2);
-    }//机器人原地旋转，可以控制旋转速度和旋转方向（顺、逆时针），但是不能控制圆周旋转角度
-    @Override
+    /*
+    * 函数名称：moveBackward
+    * 功能    ：电机反转，可以调节速度，但是超过提前标定的速度阈值则不能转动
+    */
+    public void moveBackward(int speed){
+        if( (speed >=0) && (speed <= 100)){
+            leftMoterPwmDuty1 = 0;
+            leftMoterPwmDuty2 = speed/100.0f;
+            rightMotorPwmDuty1 = 0;
+            rightMotorPwmDuty2 = speed/100.0f;
+        }
+        else if( (speed < 0) && (speed > 100)){
+            leftMoterPwmDuty1 = 0;
+            leftMoterPwmDuty2 = 0;
+            rightMotorPwmDuty1 = 0;
+            rightMotorPwmDuty2 = 0;
+        }
+    }
+    /*
+    *函数名称：turnCircle
+    *功能：原地转圈,可以实现正反转及转动速度的调节,但是速度超过提前设置的速度阈值则不能移动
+    */
+    public void turnCircle(int speed,boolean direction) {
+        if (direction){
+            if( (speed >= 0) && (speed <= 100)){
+                leftMoterPwmDuty1 = speed/100.0f;
+                leftMoterPwmDuty2 = 0;
+                rightMotorPwmDuty1 = 0;
+                rightMotorPwmDuty2 = speed/100.0f;
+            }
+            else if( (speed < 0) && (speed > 100)){
+                leftMoterPwmDuty1 = 0;
+                leftMoterPwmDuty2 = 0;
+                rightMotorPwmDuty1 = 0;
+                rightMotorPwmDuty2 = 0;
+            }
+        }
+        else {
+            if( (speed >= 0) && (speed <= 100)){
+                leftMoterPwmDuty1 = 0;
+                leftMoterPwmDuty2 = speed/100.0f;
+                rightMotorPwmDuty1 = speed/100.0f;
+                rightMotorPwmDuty2 = 0;
+            }
+            else if( (speed <= 0) && (speed >= 100)){
+                leftMoterPwmDuty1 = 0;
+                leftMoterPwmDuty2 = 0;
+                rightMotorPwmDuty1 = 0;
+                rightMotorPwmDuty2 = 0;
+            }
+
+        }
+    }
+    /*
+    *函数名称：moveStop
+    *函数功能：电机停止转动
+    */
+    public void moveStop(){
+        leftMoterPwmDuty1 = 0;
+        leftMoterPwmDuty2 = 0;
+        rightMotorPwmDuty1 = 0;
+        rightMotorPwmDuty2 = 0;
+    }
+
+    /*
+    * 函数名称：motorLeft
+    * 函数功能：左电机控制，可实现速度和旋转方向的调节，但是速度超过提前设置的速度阈值则不能移动
+     */
+    public void motorLeft(int speed,boolean direction){
+        if(direction){
+            if ((speed >= 0) && (speed <= 100)){
+                leftMoterPwmDuty1 = speed/100.0f;
+                leftMoterPwmDuty2 = 0;
+            }
+            else if ((speed < 0) && (speed > 100)) {
+                leftMoterPwmDuty1 = 0;
+                leftMoterPwmDuty2 = 0;
+            }
+        }
+        else {
+            if ((speed >= 0) && (speed <= 100)){
+                leftMoterPwmDuty1 = 0;
+                leftMoterPwmDuty2 = speed/100.0f;
+            }
+            else if ((speed < 0) && (speed > 100)) {
+                leftMoterPwmDuty1 = 0;
+                leftMoterPwmDuty2 = 0;
+            }
+        }
+    }
+    /*
+    * 函数名称：motorRight
+    * 函数功能：右电机控制，可实现速度和旋转方向的调节，但是速度超过提前设置的速度阈值则不能移动
+     */
+    public void motorRight(int speed,boolean direction){
+        if(direction){
+            if ((speed >= 0) && (speed <= 100)){
+                rightMotorPwmDuty1 = speed/100.0f;
+                rightMotorPwmDuty2 = 0;
+            }
+            else if ((speed < 0) && (speed > 100)) {
+                rightMotorPwmDuty1 = 0;
+                rightMotorPwmDuty2 = 0;
+            }
+        }
+        else {
+            if ((speed >= 0) && (speed <= 100)){
+                rightMotorPwmDuty1 = 0;
+                rightMotorPwmDuty2 = speed/100.0f;
+            }
+            else if ((speed < 0) && (speed > 100)) {
+                rightMotorPwmDuty1 = 0;
+                rightMotorPwmDuty2 = 0;
+            }
+        }
+    }
     protected IOIOLooper createIOIOLooper(){
         return new Looper();
     }
--- app/src/main/java/com/example/fuan/ioiotest/MainActivity.java
+++ app/src/main/java/com/example/fuan/ioiotest/MainActivity.java
@@ -7,266 +7,129 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
-import ioio.lib.api.DigitalInput;
-import ioio.lib.api.DigitalOutput;
-import ioio.lib.api.IOIO;
-import ioio.lib.api.PwmOutput;
-import ioio.lib.api.exception.ConnectionLostException;
-import ioio.lib.util.BaseIOIOLooper;
-import ioio.lib.util.IOIOLooper;
-import ioio.lib.util.android.IOIOActivity;
 
-
-public class MainActivity extends IOIOActivity{
+public class MainActivity extends BaseActivity {
     private TextView textView_;
     private SeekBar seekBar_;
-    private ToggleButton toggleButton_;
 
+    private ToggleButton circle;
+    private ToggleButton leftMotor;
+    private ToggleButton rightMotor;
 
-    private TextView DetectEdge;
-
+    private Button forwardButton;
+    private Button backwardButton;
+    private Button stopButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        forwardButton=(Button)findViewById(R.id.button2);
+        backwardButton=(Button)findViewById(R.id.button);
+        stopButton=(Button)findViewById(R.id.button3);
+
+        forwardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveForward(seekBar_.getProgress());
+            }
+        });
+        backwardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveBackward(seekBar_.getProgress());
+            }
+        });
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveStop();
+                circle.setText("Circle");
+                leftMotor.setText("LeftMotor");
+                rightMotor.setText("RightMotor");
+            }
+        });
+
+
+        addListenerToggleButtonCircle();
+        addListenerToggleButtonRightMotor();
+        addListenerToggleButtonLeftMotor();
+
+        addListenSeekbar();
+    }
+
+    public void addListenerToggleButtonCircle(){
+        circle=(ToggleButton)findViewById(R.id.toggleButton);
+        circle.setText("Circle");
+        circle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (circle.isChecked()) {
+                    circle.setText("左转圈");
+                    turnCircle(seekBar_.getProgress(), true);
+                } else {
+                    circle.setText("右转圈");
+                    turnCircle(seekBar_.getProgress(), false);
+                }
+            }
+        });
+    }
+    public void addListenerToggleButtonLeftMotor(){
+        leftMotor=(ToggleButton)findViewById(R.id.toggleButton2);
+        leftMotor.setText("LeftMotor");
+        leftMotor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (leftMotor.isChecked()){
+                    leftMotor.setText("LeftMotor前转");
+                    motorLeft(seekBar_.getProgress(), false);
+                }
+                else {
+                    leftMotor.setText("LeftMotor后转");
+                    motorLeft(seekBar_.getProgress(), true);
+                }
+            }
+        });
+    }
+    public void addListenerToggleButtonRightMotor(){
+        rightMotor=(ToggleButton)findViewById(R.id.toggleButton3);
+        rightMotor.setText("RightMotor");
+        rightMotor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (rightMotor.isChecked()){
+                    rightMotor.setText("RightMotor前转");
+                    motorRight(seekBar_.getProgress(), false);
+
+                }
+                else {
+                    rightMotor.setText("RightMotor后转");
+                    motorRight(seekBar_.getProgress(), true);
+                }
+            }
+        });
+    }
+    public void addListenSeekbar(){
         textView_=(TextView)findViewById(R.id.textView2);
         seekBar_=(SeekBar)findViewById(R.id.seekBar);
-        toggleButton_=(ToggleButton)findViewById(R.id.toggleButton);
-
-
-
-        DetectEdge=(TextView)findViewById(R.id.detectedge);//红外检测变化时文字框发生改变
-
         seekBar_.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 textView_.setText(seekBar_.getProgress() + "%");
             }
-
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
 
             }
-
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
 
             }
         });
-
-        enableUi(false);
-    }
-    class Looper extends BaseIOIOLooper{
-        private PwmOutput PwmOutput_1;
-        private DigitalOutput MotorRefer_1;//1号电机
-        private PwmOutput PwmOutput_2;
-        private DigitalOutput MotorRefer_2;//2号电机
-        private float pwmChange;
-        private Boolean motorRotate_1=true;
-        private Boolean motorRotate_2=true;
-
-//      private DigitalOutput led_;
-
-        private DigitalOutput LedLefteye;
-        private DigitalOutput LedRighteye;
-        private DigitalOutput LedMouth;//嘴巴、眼睛处的led灯
-        private Boolean LedLefteyeswitch=true;
-        private Boolean LedRighteyeswitch=true;
-        private Boolean LedMouthswitch=true;
-
-        private DigitalInput DetectEdge;//边缘检测输入引脚变量设置
-        private Boolean DetectEdgeflag=true;//边缘检测标志位
-
-        private DigitalInput TouchHead;
-        private DigitalInput TouchShlf;
-        private DigitalInput TouchShrg;
-        private DigitalInput TouchBottom;//触摸部位引脚变量设置
-        private Boolean TouchHeadflag=true;
-        private Boolean TouchShlfflag=true;
-        private Boolean TouchShrgflag=true;
-        private Boolean TouchBottomflag=true;
-
-        @Override
-        public void setup() throws ConnectionLostException{
-            //led_=ioio_.openDigitalOutput(IOIO.LED_PIN, true);
-
-            PwmOutput_1=ioio_.openPwmOutput(11, 100);
-            MotorRefer_1=ioio_.openDigitalOutput(12, true);
-            PwmOutput_2=ioio_.openPwmOutput(13,100);
-            MotorRefer_2=ioio_.openDigitalOutput(14,true);//电机引脚设置
-
-            LedLefteye=ioio_.openDigitalOutput(16, true);
-            LedRighteye=ioio_.openDigitalOutput(17,true);
-            LedMouth=ioio_.openDigitalOutput(18, true);//左右眼、嘴巴处Led灯的引脚设置
-
-
-            //touchSen_=ioio_.openDigitalInput(10, DigitalInput.Spec.Mode.PULL_DOWN);
-
-            DetectEdge=ioio_.openDigitalInput(20);//边缘检测输入管脚设置
-
-            TouchHead=ioio_.openDigitalInput(25);
-            TouchShlf=ioio_.openDigitalInput(26);
-            TouchShrg=ioio_.openDigitalInput(27);
-            TouchBottom=ioio_.openDigitalInput(28);//触摸部位输入管脚设置
-
-            enableUi(true);
-
-        }
-
-        public void MoveForward(int speed) throws ConnectionLostException,InterruptedException{
-            pwmChange = speed/100.0f;
-            motorRotate_1=true;
-            motorRotate_2=true;
-            PwmOutput_1.setDutyCycle(pwmChange);
-            MotorRefer_1.write(motorRotate_1);
-            PwmOutput_2.setDutyCycle(pwmChange);
-            MotorRefer_2.write(motorRotate_2);
-        }//电机正转
-
-        public void Backward(int speed) throws ConnectionLostException,InterruptedException{
-            pwmChange = speed/100.0f;
-            motorRotate_1=false;
-            motorRotate_2=false;
-            PwmOutput_1.setDutyCycle(pwmChange);
-            MotorRefer_1.write(motorRotate_1);
-            PwmOutput_2.setDutyCycle(pwmChange);
-            MotorRefer_2.write(motorRotate_2);
-        }//电机反转
-
-        public void TurnCircle(int speed,boolean direction) throws ConnectionLostException,InterruptedException{
-            pwmChange = speed/100.0f;
-            motorRotate_1 = direction;
-            motorRotate_2 = !direction;
-            PwmOutput_1.setDutyCycle(pwmChange);
-            MotorRefer_1.write(!motorRotate_1);
-            PwmOutput_2.setDutyCycle(pwmChange);
-            MotorRefer_2.write(motorRotate_2);
-        }//机器人原地旋转，可以控制旋转速度和旋转方向（顺、逆时针），但是不能控制圆周旋转角度
-
-        public void Stop() throws ConnectionLostException,InterruptedException{
-            if (motorRotate_1){
-                pwmChange=1.0f;
-            }
-            else {
-                pwmChange=0.0f;
-            }
-            PwmOutput_1.setDutyCycle(pwmChange);
-            if (motorRotate_2){
-                pwmChange=1.0f;
-            }
-            else {
-                pwmChange=0.0f;
-            }
-            PwmOutput_2.setDutyCycle(pwmChange);
-        }//不管机器人在哪个运动状态，都可以使机器人停止
-
-        public void Led(boolean lefteye,boolean righteye,boolean mouth)throws  ConnectionLostException,InterruptedException{
-            LedLefteyeswitch=lefteye;
-            LedLefteye.write(LedLefteyeswitch);
-
-            LedRighteyeswitch=righteye;
-            LedRighteye.write(LedRighteyeswitch);
-
-            LedMouthswitch=mouth;
-            LedMouth.write(LedMouthswitch);
-        }//眼睛和嘴巴的Led开关控制函数，顺序为：左眼，右眼，嘴巴
-
-        public void Detect() throws ConnectionLostException,InterruptedException{
-            if (DetectEdge.read())
-                DetectEdgeflag=true;
-            else
-                DetectEdgeflag=false;
-
-        }//检测到边缘的时候（即无障碍物），红外管输出高电平信号
-
-        public void Touch() throws ConnectionLostException,InterruptedException{
-            if (TouchHead.read())
-                TouchHeadflag = true;
-            else
-                TouchHeadflag = false;
-
-            if (TouchShlf.read())
-                TouchShlfflag = true;
-            else
-                TouchShlfflag = false;
-
-            if (TouchShrg.read())
-                TouchShrgflag = true;
-            else
-                TouchShrgflag = false;
-
-            if (TouchBottom.read())
-                TouchBottomflag = true;
-            else
-                TouchBottomflag = false;
-        }//如果头部、左右耳朵及底部触摸检测成功，相应标志位置高
-
-        @Override
-        public void loop() throws ConnectionLostException,InterruptedException{
-
-            //pwmOutput_.setDutyCycle(seekBar_.getProgress() / 100.0f);
-            //pwmOutput_.setDutyCycle(0.5f);
-            //textView_.setText(seekBar_.getProgress()+"%");
-            // led_.write(!toggleButton_.isChecked());
-            /**if(touchSen_.read())
-             led_.write(true);
-             else
-             led_.write(false);*/
-
-        }
-
-        @Override
-        public void disconnected(){
-            enableUi(false);
-        }
-
-    }
-
-    @Override
-    protected IOIOLooper createIOIOLooper(){
-        return new Looper();
-    }
-
-    private void enableUi(final boolean enable){
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                seekBar_.setEnabled(enable);
-                toggleButton_.setEnabled(enable);
-            }
-        });
     }
 }
-
-/*public class MainActivity extends ActionBarActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-}*/
