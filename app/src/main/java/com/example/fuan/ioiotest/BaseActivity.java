02904efcc964b32e728851ad0947311a0d233b45
zhengyang <718350171@qq.com>
2016-02-27 14:03:11 +0800

    更新了MainActivity的控制UI界面，创建了超声波测距模块的运行线程，参照安卓编码规范更新了BaseActivity中的代码风格

--- app/src/main/java/com/example/fuan/ioiotest/BaseActivity.java
+++ app/src/main/java/com/example/fuan/ioiotest/BaseActivity.java
@@ -1,34 +1,42 @@
 package com.example.fuan.ioiotest;
 
+import android.util.Log;
 
 import ioio.lib.api.DigitalInput;
 import ioio.lib.api.DigitalOutput;
+import ioio.lib.api.PulseInput;
 import ioio.lib.api.PwmOutput;
-import ioio.lib.api.DigitalOutput.Spec.Mode;
 import ioio.lib.api.exception.ConnectionLostException;
 import ioio.lib.util.BaseIOIOLooper;
 import ioio.lib.util.IOIOLooper;
 import ioio.lib.util.android.IOIOActivity;
+import ioio.lib.api.IOIO;
 
-
-public class
-        BaseActivity extends IOIOActivity{
+public class BaseActivity extends IOIOActivity{
+    /**
+     * 左右电机的Pwm波和占空比变量设置
+     * Pwm_Pin设置Pwm波输出引脚
+     */
     private PwmOutput leftMotorPwm1,leftMotorPwm2,rightMotorPwm1,rightMotorPwm2;
     private float leftMoterPwmDuty1,leftMoterPwmDuty2,rightMotorPwmDuty1,rightMotorPwmDuty2;
-    private final static int PwmPin1=11;
-    private final static int PwmPin2=12;
-    private final static int PwmPin3=13;
-    private final static int PwmPin4=14;
-    /*
-    detectEdge为红外模块输入变量，用于边缘检测
-    detectEdgeEnable为边缘检测开关，true时为开启量，false时为关闭量
+    private final static int Pwm_Pin1=11;
+    private final static int Pwm_Pin2=12;
+    private final static int Pwm_Pin3=13;
+    private final static int Pwm_Pin4=14;
+
+    /**
+     * detectEdge为边缘检测模块输入变量
+     * detectEdgeEnable为边缘检测开关，true时为开启量，false时为关闭量
+     * DetectEdge_Pin设置边缘检测模块的引脚
      */
     private DigitalInput detectEdge;
     private Boolean detectEdgeEnable=false;
-    private final static int DetectEdgePin=29;
-    /*
-    touchSence为触摸传感器输入变量
-    touchSencePlace全局变量记录发什么触摸的位置
+    private final static int DetectEdge_Pin=29;
+
+    /**
+     * touchSence为触摸传感器输入变量
+     * touchSencePlace全局变量记录发什么触摸的位置
+     * TouchSence_Pin设置触摸检测模块的引脚
      */
     private DigitalInput touchSence1;
     private DigitalInput touchSence2;
@@ -36,13 +44,16 @@ public class
     private DigitalInput touchSence4;
     private DigitalInput touchSence5;
     private int touchSencePlace;
-    private final static int TouchSencePin1=22;
-    private final static int TouchSencePin2=23;
-    private final static int TouchSencePin3=24;
-    private final static int TouchSencePin4=25;
-    private final static int TouchSencePin5=26;
-    /*
-    faceLed为Led输出变量
+    private final static int TouchSence_Pin1=22;
+    private final static int TouchSence_Pin2=23;
+    private final static int TouchSence_Pin3=24;
+    private final static int TouchSence_Pin4=25;
+    private final static int TouchSence_Pin5=26;
+
+    /**
+     * faceLed为头部Led输出变量
+     * faceLedFlag为Led使能标志
+     * FaceLed_Pin设置头部Led输出引脚
      */
     private DigitalOutput faceLedLeftEye;
     private DigitalOutput faceLedRightEye;
@@ -50,65 +61,113 @@ public class
     private Boolean faceLedLeftEyeFlag;
     private Boolean faceLedRightEyeFlag;
     private Boolean faceLedMouthFlag;
-    private final static int FaceLedPin1=1;
-    private final static int FaceLedPin2=2;
-    private final static int FaceLedPin3=3;
-    /*
-    servoPwm为舵机Pwm波输出变量
+    private final static int FaceLed_Pin1=1;
+    private final static int FaceLed_Pin2=2;
+    private final static int FaceLed_Pin3=3;
+
+    /**
+     * servoPwm为舵机Pwm波输出变量
+     * servoPwmDuty为舵机Pwm波占空比变量
+     * Servo_Pin设置舵机Pwm波输出引脚
      */
     private PwmOutput servoPwm1,servoPwm2;
     private float servoPwmDuty1,servoPwmDuty2;
-    private final static int ServoPin1=6;
-    private final static int ServoPin2=10;
+    private final static int Servo_Pin1=5;
+    private final static int Servo_Pin2=10;
 
+    /**
+     * ultrasonicSensorTrigger为超生波探测器触发数字输出信号变量
+     * ultrasonicSensorEcho为超声波探测器回声检测持续时间输出口
+     * ultrasonicSensor_Pin设置超声波探测器的连接引脚
+     */
+    private DigitalOutput ultrasonicSensorTrigger;
+    private PulseInput ultrasonicSensorEcho;
+    private final static int ultrasonicSensorTrigger_Pin=7;
+    private final static int ultrasonicSensorEcho_Pin=6;
+    private IOIO ioio_;
+    public int echoSeconds;
+    public int echoDistanceCm;
+
+    /**
+     * 这个线程在所有的IOIO Activity中都会出现，一旦应用启动它会重新开始运行，并且在应用暂停的时候
+     * 会终止此线程。
+     *
+     * initMotor()方法用于配置电机模块
+     * initDetectEdge()方法用于配置边缘检测模块
+     * initTouchSence()方法用于配置触摸模块
+     * initLed()方法用于配置Led模块
+     * initServo()方法用于配置舵机模块
+     * initUltrasonicSensor()方法用于配置超声波传感器模块
+     */
     class Looper extends BaseIOIOLooper{
-        /*
-        * 函数名称：initMotor
+        /**
+        * 方法名称：initMotor（）
         * 功能：电机引脚GPIO口设置
         */
         public void initMotor() throws ConnectionLostException,InterruptedException{
-            leftMotorPwm1 = ioio_.openPwmOutput(PwmPin1, 100);
-            leftMotorPwm2 = ioio_.openPwmOutput(PwmPin2, 100);
-            rightMotorPwm1 = ioio_.openPwmOutput(PwmPin3, 100);
-            rightMotorPwm2 = ioio_.openPwmOutput(PwmPin4, 100);
+            leftMotorPwm1 = ioio_.openPwmOutput(Pwm_Pin1, 100);
+            leftMotorPwm2 = ioio_.openPwmOutput(Pwm_Pin2, 100);
+            rightMotorPwm1 = ioio_.openPwmOutput(Pwm_Pin3, 100);
+            rightMotorPwm2 = ioio_.openPwmOutput(Pwm_Pin4, 100);
         }
-        /*
-        * 函数名称：initDetectEdge()
+
+        /**
+        * 方法名称：initDetectEdge()
         * 功能：红外模块引脚GPIO口设置
         */
         public void initDetectEdge() throws ConnectionLostException,InterruptedException{
-            detectEdge = ioio_.openDigitalInput(DetectEdgePin);
+            detectEdge = ioio_.openDigitalInput(DetectEdge_Pin);
 
         }
-        /*
-        * 函数名称：initTouchSence()
+
+        /**
+        * 方法名称：initTouchSence()
         * 功能：触摸模块引脚GPIO口设置
         */
         public void initTouchSence() throws ConnectionLostException,InterruptedException{
-            touchSence1 = ioio_.openDigitalInput(TouchSencePin1);
-            touchSence2 = ioio_.openDigitalInput(TouchSencePin2);
-            touchSence3 = ioio_.openDigitalInput(TouchSencePin3);
-            touchSence4 = ioio_.openDigitalInput(TouchSencePin4);
-            touchSence5 = ioio_.openDigitalInput(TouchSencePin5);
+            touchSence1 = ioio_.openDigitalInput(TouchSence_Pin1);
+            touchSence2 = ioio_.openDigitalInput(TouchSence_Pin2);
+            touchSence3 = ioio_.openDigitalInput(TouchSence_Pin3);
+            touchSence4 = ioio_.openDigitalInput(TouchSence_Pin4);
+            touchSence5 = ioio_.openDigitalInput(TouchSence_Pin5);
         }
-        /*
-        * 函数名称：initLed()
+
+        /**
+        * 方法名称：initLed()
         * 功能：Led模块引脚GPIO口设置
         */
         public void initLed() throws ConnectionLostException,InterruptedException{
-            faceLedLeftEye = ioio_.openDigitalOutput(FaceLedPin1, false);
-            faceLedRightEye = ioio_.openDigitalOutput(FaceLedPin2, false);
-            faceLedMouth = ioio_.openDigitalOutput(FaceLedPin3, false);
+            faceLedLeftEye = ioio_.openDigitalOutput(FaceLed_Pin1, false);
+            faceLedRightEye = ioio_.openDigitalOutput(FaceLed_Pin2, false);
+            faceLedMouth = ioio_.openDigitalOutput(FaceLed_Pin3, false);
         }
-        /*
-        * 函数名称：initServo()
+
+        /**
+        * 方法名称：initServo()
         * 功能:舵机模块引脚GPIO口设置
          */
         public void initServo() throws ConnectionLostException,InterruptedException{
-            servoPwm1 = ioio_.openPwmOutput(ServoPin1, 50);
-            servoPwm2 = ioio_.openPwmOutput(ServoPin2, 50);
+            servoPwm1 = ioio_.openPwmOutput(Servo_Pin1, 50);
+            servoPwm2 = ioio_.openPwmOutput(Servo_Pin2, 50);
         }
 
+        /**
+        * 方法名称：initUltrasonicSensor()
+        * 功能：超声波传感器引脚的GPIO口设置
+         */
+        public void initUltrasonicSensor() throws ConnectionLostException{
+           try{
+               ultrasonicSensorEcho=ioio_.openPulseInput(ultrasonicSensorEcho_Pin, PulseInput.PulseMode.POSITIVE);
+               ultrasonicSensorTrigger=ioio_.openDigitalOutput(ultrasonicSensorTrigger_Pin);
+           }catch (ConnectionLostException e){
+               throw e;
+           }
+        }
+
+        /**
+         * 方法名称：setup()
+         * 功能：在当前线程中对各个模块进行初始化
+         */
         @Override
         public void setup() throws ConnectionLostException,InterruptedException{
             initMotor();
@@ -116,9 +175,13 @@ public class
             initTouchSence();
             initLed();
             initServo();
+            initUltrasonicSensor();
+            UltrasonicsSensorOutput us =new UltrasonicsSensorOutput();
+            new Thread(us).start(); //超声波测距传感器的线程实例化后放在这里启动
         }
-        /*
-        * 函数名称：setMotorDuty
+
+        /**
+        * 方法名称：setMotorDuty
         * 功能：电机pwm波参数设置
         */
         public void setMotorDuty()  throws ConnectionLostException,InterruptedException{
@@ -127,16 +190,18 @@ public class
             rightMotorPwm1.setDutyCycle(rightMotorPwmDuty1);
             rightMotorPwm2.setDutyCycle(rightMotorPwmDuty2);
         }
-        /*
-        * 函数名称：setServoDuty()
+
+        /**
+        * 方法名称：setServoDuty()
         * 功能：舵机pwm波参数设置
         */
         public void setServoDuty() throws ConnectionLostException, InterruptedException {
             servoPwm1.setDutyCycle(servoPwmDuty1);
             servoPwm2.setDutyCycle(servoPwmDuty2);
         }
-        /*
-        * 函数名称：judgeTouchPlace()
+
+        /**
+        * 方法名称：judgeTouchPlace()
         * 功能：依次读取触摸传感器的各GPIO口电平，然后在触摸事件发生时判断发生触摸的位置，用touchSencePlace来记录
         */
         public void judgeTouchPlace() throws ConnectionLostException,InterruptedException{
@@ -151,9 +216,10 @@ public class
             if (touchSence5.read())
                 touchSencePlace=5;
         }
-        /*
-        函数名称：setDetectEdge()
-        功能：判断机器人的运动方向上是否存在跌落的危险
+
+        /**
+        * 方法名称：setDetectEdge()
+        * 功能：判断机器人的运动方向上是否存在跌落的危险
          */
         public void setDetectEdge() throws ConnectionLostException,InterruptedException{
             if(detectEdgeEnable){
@@ -162,8 +228,9 @@ public class
                 }
             }
         }
-        /*
-        * 函数名称：setFaceLedPlace(boolean lefteye,boolean rigtheye,boolean mouth)
+
+        /**
+        * 方法名称：setFaceLedPlace()
         * 功能：设置Led灯的位置并且控制灯的亮灭
         */
         public void setFaceLedPlace() throws ConnectionLostException,InterruptedException{
@@ -171,23 +238,68 @@ public class
             faceLedRightEye.write(faceLedRightEyeFlag);
             faceLedMouth.write(faceLedMouthFlag);
         }
+
+        /**
+         * 当IOIO连接建立的时候反复调用loop（）方法
+         *
+         * @throws ConnectionLostException
+         *         当IOIO连接丢失的时候
+         * @throws InterruptedException
+         *         当IOIO线程中断的时候
+         */
         @Override
-        public void loop() throws ConnectionLostException,InterruptedException{
+        public void loop() throws ConnectionLostException,InterruptedException {
             setMotorDuty();
             judgeTouchPlace();
             setDetectEdge();
             setServoDuty();
+            Log.d("Thread","motor run");
         }
 
+        /**
+         * 当和IOIO失去连接的时候调用此方法
+         */
         @Override
         public void disconnected(){
 
         }
     }
-    /*
-    * 函数名称：moveForward
-    * 功能：电机正转，可以调节速度，但是超过提前标定的速度阈值则不能转动
-    */
+
+    /**
+     * 此方法创建了超声波测距模块单独运行的线程
+     *
+     * run()内部为线程运行时的执行体
+     */
+    class UltrasonicsSensorOutput implements Runnable{
+        @Override
+        public void run() {
+            while(true)
+            {
+                try {
+                    ultrasonicSensorTrigger.write(false);
+                    Thread.sleep(5);
+                    ultrasonicSensorTrigger.write(true);
+                    Thread.sleep(1);
+                    ultrasonicSensorTrigger.write(false);
+                    echoSeconds=(int)(ultrasonicSensorEcho.getDuration()*1000*1000);
+                    echoDistanceCm=echoSeconds/29/2;
+                    Thread.sleep(20);
+                    Log.d("Thread", "get the data");//超生波测距线程循环执行的时候可以打印此日志
+                }catch (InterruptedException e){
+                    ioio_.disconnect();
+                }catch (ConnectionLostException e){
+                }
+            }
+        }
+
+    }
+
+    /**
+     * 方法名称：moveForward(int speed)
+     * 功能：电机正转
+     *
+     * @param speed 调节速度
+     */
     public void moveForward(int speed){
         if( (speed >=0) && (speed <= 100)){
             leftMoterPwmDuty1 = speed/100.0f;
@@ -203,10 +315,13 @@ public class
         }
 
     }
-    /*
-    * 函数名称：moveBackward
-    * 功能    ：电机反转，可以调节速度，但是超过提前标定的速度阈值则不能转动
-    */
+
+    /**
+     * 方法名称：moveBackward（int speed）
+     * 功能：电机反转
+     *
+     * @param speed 调节速度
+     */
     public void moveBackward(int speed){
         if( (speed >=0) && (speed <= 100)){
             leftMoterPwmDuty1 = 0;
@@ -221,10 +336,14 @@ public class
             rightMotorPwmDuty2 = 0;
         }
     }
-    /*
-    *函数名称：turnCircle
-    *功能：原地转圈,可以实现正反转及转动速度的调节,但是速度超过提前设置的速度阈值则不能移动
-    */
+
+    /**
+     * 方法名称：turnCircle(int speed,boolean direction)
+     * 功能：原地转圈
+     *
+     * @param speed 调节速度
+     * @param direction 调节转动方向
+     */
     public void turnCircle(int speed,boolean direction) {
         if (direction){
             if( (speed >= 0) && (speed <= 100)){
@@ -256,9 +375,10 @@ public class
 
         }
     }
-    /*
-    *函数名称：moveStop
-    *功能：电机停止转动
+
+    /**
+    * 方法名称：moveStop（）
+    * 功能：电机停止转动
     */
     public void moveStop(){
         leftMoterPwmDuty1 = 0;
@@ -267,9 +387,11 @@ public class
         rightMotorPwmDuty2 = 0;
     }
 
-    /*
-    * 函数名称：motorLeft
-    * 功能：左电机控制，可实现速度和旋转方向的调节，但是速度超过提前设置的速度阈值则不能移动
+    /**
+     * 方法名称：motorLeft(int speed,boolean direction)
+     * 功能：左电机控制
+     * @param speed 调节速度
+     * @param direction 调节转动方向
      */
     public void motorLeft(int speed,boolean direction){
         if(direction){
@@ -293,9 +415,13 @@ public class
             }
         }
     }
-    /*
-    * 函数名称：motorRight
-    * 功能：右电机控制，可实现速度和旋转方向的调节，但是速度超过提前设置的速度阈值则不能移动
+
+    /**
+     * 方法名称：motorRight(int speed,boolean direction)
+     * 功能：右电机控制
+     *
+     * @param speed 调节速度
+     * @param direction 调节转动方向
      */
     public void motorRight(int speed,boolean direction){
         if(direction){
@@ -319,6 +445,12 @@ public class
             }
         }
     }
+
+    /**
+     * 方法名称：openLed(int ledPlace)
+     * 功能：开启头部Led
+     * @param ledPlace Led点亮的位置
+     */
     public void openLed(int ledPlace) {
         if ((ledPlace >= 0 )&&(ledPlace < 8)){
             switch (ledPlace){
@@ -373,15 +505,22 @@ public class
             }
         }
     }
-    /*
-    * 函数名称：steeringEngine
-    * 功能：可以实现舵机180°转动调节，speed=0时，电机占空比设为2.5，此时对应的转动角度为0，发生舵机的抖动，
-    * 所以设为2.56，让电机转动角度初值比0大一点
+
+    /**
+     * 方法名称：servoControl(float angle)
+     * 功能：可以实现舵机180°转动调节，speed=0时，电机占空比设为2.5，此时对应的转动角度为0，发生舵机的抖动，
+     * 所以设为2.56，让电机转动角度初值比0大一点
+     *
+     * @param angle 调整舵机转动到达的角度
      */
     public void servoControl(float angle){
         servoPwmDuty1=(2.56f+angle/10)/100;
         servoPwmDuty2=(2.56f+angle/10)/100;
     }
+
+    /**
+     * 创建自己的IOIO线程的方法
+     */
     protected IOIOLooper createIOIOLooper(){
         return new Looper();
     }
--- app/src/main/java/com/example/fuan/ioiotest/MainActivity.java
+++ app/src/main/java/com/example/fuan/ioiotest/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.fuan.ioiotest;
 
-
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.ToggleButton;
-
 
 public class MainActivity extends BaseActivity {
     private TextView textView_;
@@ -21,15 +21,21 @@ public class MainActivity extends BaseActivity {
     private Button backwardButton;
     private Button stopButton;
 
+    private TextView ultrasonicsSensorText;
+    private ProgressBar ultrasonicsSensorPrograss;
+    private ToggleButton ultrasonicsSensorButton;
+
+    private volatile Boolean flag = true;
     @Override
-    public void onCreate(Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-        forwardButton=(Button)findViewById(R.id.button2);
-        backwardButton=(Button)findViewById(R.id.button);
-        stopButton=(Button)findViewById(R.id.button3);
+        forwardButton = (Button) findViewById(R.id.button2);
+        backwardButton = (Button) findViewById(R.id.button);
+        stopButton = (Button) findViewById(R.id.button3);
+
 
         forwardButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,11 +66,21 @@ public class MainActivity extends BaseActivity {
 
         addListenSeekbar();
 
+        ultrasonicsSensorButton = (ToggleButton) findViewById(R.id.button6);
+        ultrasonicsSensorPrograss = (ProgressBar) findViewById(R.id.progressBar);
+        ultrasonicsSensorText = (TextView) findViewById(R.id.textSet8);
+        addListenerUltrasonicsSensor();
+
+        Log.d("Ultrasonic", "the final output");
+
         //addListenDetectEdge();
     }
 
-    public void addListenerToggleButtonCircle(){
-        circle=(ToggleButton)findViewById(R.id.toggleButton);
+    /**
+     * 电机转圈控制按钮
+     */
+    public void addListenerToggleButtonCircle() {
+        circle = (ToggleButton) findViewById(R.id.toggleButton);
         circle.setText("Circle");
         circle.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -80,46 +96,56 @@ public class MainActivity extends BaseActivity {
             }
         });
     }
-    public void addListenerToggleButtonLeftMotor(){
-        leftMotor=(ToggleButton)findViewById(R.id.toggleButton2);
+
+    /**
+     * 左电机控制按钮
+     */
+    public void addListenerToggleButtonLeftMotor() {
+        leftMotor = (ToggleButton) findViewById(R.id.toggleButton2);
         leftMotor.setText("LeftMotor");
         leftMotor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if (leftMotor.isChecked()){
+                if (leftMotor.isChecked()) {
                     leftMotor.setText("LeftMotor前转");
                     motorLeft(seekBar_.getProgress(), false);
-                }
-                else {
+                } else {
                     leftMotor.setText("LeftMotor后转");
                     motorLeft(seekBar_.getProgress(), true);
                 }
             }
         });
     }
-    public void addListenerToggleButtonRightMotor(){
-        rightMotor=(ToggleButton)findViewById(R.id.toggleButton3);
+
+    /**
+     * 右电机控制按钮
+     */
+    public void addListenerToggleButtonRightMotor() {
+        rightMotor = (ToggleButton) findViewById(R.id.toggleButton3);
         rightMotor.setText("RightMotor");
         rightMotor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if (rightMotor.isChecked()){
+                if (rightMotor.isChecked()) {
                     rightMotor.setText("RightMotor前转");
                     motorRight(seekBar_.getProgress(), false);
 
-                }
-                else {
+                } else {
                     rightMotor.setText("RightMotor后转");
                     motorRight(seekBar_.getProgress(), true);
                 }
             }
         });
     }
-    public void addListenSeekbar(){
-        textView_=(TextView)findViewById(R.id.textView2);
-        seekBar_=(SeekBar)findViewById(R.id.seekBar);
+
+    /**
+     * 电机速度和舵机角度设置进度条
+     */
+    public void addListenSeekbar() {
+        textView_ = (TextView) findViewById(R.id.textView2);
+        seekBar_ = (SeekBar) findViewById(R.id.seekBar);
         seekBar_.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
@@ -138,8 +164,41 @@ public class MainActivity extends BaseActivity {
 
             }
         });
+    };
+
+    /**
+     * 超声波测距监听按钮
+     */
+    public void addListenerUltrasonicsSensor(){
+        ultrasonicsSensorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ultrasonicsSensorButton.isChecked()) {
+                    updateUI();
+                }
+                else{
+                    updateUI();
+                }
+            }
+        });
     }
+
+    /**
+     * 用于接收超声波测距的数据并更新UI界面
+     */
+    public void updateUI(){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                ultrasonicsSensorText.setText(String.valueOf(echoDistanceCm));
+                ultrasonicsSensorPrograss.setProgress(echoSeconds);
+                Log.d("UI","receive the data");
+            }
+        });
+    }
+
 }
+
 
 
 
